7cce4b16d1aac2423fe9fedae3501af0d0d42af3
Oscar Castillo <ocp529@gmail.com>
2025-03-12 15:22:46 -0500

    update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,7 +5,7 @@ from source.excel_creation import ExcelPackages
 
 class TestPackages(unittest.TestCase):
     def setUp(self):
-        self.package = Packages("Box 1", "2", "5", "express", ["Book"], ["Education"], ["10x10x10 cm"], "pending")
+        self.package = Packages("Box 1", "2", "5", "express", "Book", "Education", "10x10x10 cm", "pending")
 
     def test_modify_package(self):
         self.package.name = "Box 2"
@@ -17,14 +17,14 @@ class TestPackages(unittest.TestCase):
 
         self.assertEqual(self.package.name, "Box 2")
         self.assertEqual(self.package.weight, "3 kg")
-        self.assertEqual(self.package.content, ["Notebook"])  # Se mantiene como lista simple
-        self.assertEqual(self.package.category, ["Office"])
-        self.assertEqual(self.package.dimension, ["20x20x20 cm"])
+        self.assertEqual(self.package.content, "Notebook")  # Se mantiene como lista simple
+        self.assertEqual(self.package.category, "Office")
+        self.assertEqual(self.package.dimension, "20x20x20 cm")
         self.assertEqual(self.package.order_status, "shipped")
 
 class TestExcelPackages(unittest.TestCase):
     def test_save_packages(self):
-        package = Packages("Box Test", "1", "50", "standard", "Mouse", ["Technology"], ["5x5x5 cm"], "pending")
+        package = Packages("Box Test", "1", "50", "standard", "Mouse", "Technology", "5x5x5 cm", "pending")
         ExcelPackages.save(package)
 
         wb = openpyxl.load_workbook(ExcelPackages.FILE_PATH)
